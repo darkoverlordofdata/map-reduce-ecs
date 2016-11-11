7611047b842797135d8e4c2e4ed6ec6ac185c906
darkoverlordofdata <darkoverlordofdata@gmail.com>
2016-11-10 23:43:57 -0800

    daily
    daily

--- FSharpGame/Program.fs
+++ FSharpGame/Program.fs
@@ -1,8 +1,15 @@
-﻿open ShmupWarzGame
+﻿open System
+open ShmupWarzGame
+
 
 [<EntryPoint>]
 let main argv = 
 //    use game = new ShmupWarz(600, 400, false)
-    use game = new ShmupWarz(900, 600, false)
+    let height = 750
+    let width = float32 height / 1.5f
+    use game = new ShmupWarz(int height, int width, false)
     game.Run()
     0
+
+
+
